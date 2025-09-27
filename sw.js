d70edfbae0bdfdaab1fa4a3d9d55f2ff814086d1
Zac0511 <zac0511_le_vrai@proton.me>
2025-09-27 16:11:54 +0200

    Add service worker

---
+++ sw.js
@@ -0,0 +1,18 @@
+// This file allows the PWA to work offline
+
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
